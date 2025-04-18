47aba8e80061d70910ea00d0f6e58b6ad8e622cb
Anh Tuan <LeTuan752003@gmail.com>
2025-04-18 14:49:29 +0700

    create table crawl_tasks and business_data

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# user postgres, password 07052003, database trangvang_db
+# user postgres, password 07052003, database trangvang_db, table crawl_tasks, business_data
 DATABASE_URL = "postgresql+psycopg2://postgres:07052003@localhost:5432/trangvang_db"
 
 engine = create_engine(DATABASE_URL)
